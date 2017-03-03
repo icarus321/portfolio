99e340d6f8fb61410653b06496d54b5c505c5d0f
bobKnowsBest <james-madden@uiowa.edu>
2017-03-03 03:34:19 -0600

    Add files via upload

---
+++ formAutomation/formAutomation.py
@@ -0,0 +1,30 @@
+#this program is for automating logins to useraccounts
+
+import urllib, webbrowser
+import urllib2
+from urllib import urlencode
+new = 2
+
+myName =  "username"
+myPassword = "password"
+data1 = {
+        "formId1" : myName,
+        "formId2" : myPassword,
+       
+        "FormSubmitId" : " Log In " 
+          
+            
+        }
+encoded_data1 = urllib.urlencode(data1)
+url1 = "the url to connect to" + urlencode(data1)
+
+#alternative approach is to just include the username and password in the string with ?, & and = characters
+url2 = "the url?formId1=username&formId2=password&FormSubmitId= Log In"
+
+#code below makes the connection and opens the page
+webbrowser.open(url2, new=2, autoraise=True);
+
+i = 0;
+
+
+
