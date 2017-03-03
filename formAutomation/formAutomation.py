b3158f8d9c3a1ccecb5565059fd3cb5620fcd1c1
bobKnowsBest <james-madden@uiowa.edu>
2017-03-03 03:56:53 -0600

    Update formAutomation.py

--- formAutomation/formAutomation.py
+++ formAutomation/formAutomation.py
@@ -18,11 +18,16 @@ data1 = {
 encoded_data1 = urllib.urlencode(data1)
 url1 = "the url to connect to" + urlencode(data1)
 
+webbrowser.open(url1, new=2, autoraise=True)
 #alternative approach is to just include the username and password in the string with & and = characters
 url2 = "the url&formId1=username&formId2=password&FormSubmitId= Log In"
 
 #code below makes the connection and opens the page
 webbrowser.open(url2, new=2, autoraise=True)
+
+#if multiple accounts are to be include you may find it necessary to put some iterator or time function
+#program execution is fast enough to force the browser to open new windows rather than new tabs
+#a simple loop will serve this added requirement
 
 
 
